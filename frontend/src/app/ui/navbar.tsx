9340fa377505949d77ba4cc740a4149c79a12574
suvesh <suveshgurung2@gmail.com>
2025-03-27 12:36:56 +0545

    added files for login and signup page and connected the navbar

--- frontend/src/app/ui/navbar.tsx
+++ frontend/src/app/ui/navbar.tsx
@@ -1,9 +1,14 @@
 import Navlinks from '@/app/ui/nav-links';
+import Link from 'next/link';
 
 export default function Navbar() {
   return(
-    <header className='h-full flex flex-row justify-around py-4'>
-      This is where the logo goes.
+    <header className='h-full flex flex-row justify-around py-4 bg-white/80 backdrop-blur-sm border-b border-gray-200 shadow-sm'>
+      <Link
+      href="/"
+      >
+        This is where the logo goes.
+      </Link>
       <Navlinks />
     </header>
   );
