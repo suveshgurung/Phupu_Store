5ebeca18f15bea1abb18280f3616b2fd2611db51
suvesh <suveshgurung2@gmail.com>
2025-04-01 21:43:14 +0545

    backend database error not fixed

---
+++ backend/controllers/auth-controller.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+const login = (req, res, next) => {
+};
+
+const signup = (req, res, next) => {
+};
+
+const logout = (req, res, next) => {
+};
+
+module.exports = {
+  login,
+  signup,
+  logout,
+};
